02e421106281ec864228a2ac97008cd7168c42f6
nisha124-svg <bhattarainisha548@gmail.com>
2025-11-04 15:42:44 +0545

    Task 2

---
+++ Task1/Task 2/Program.cs
@@ -0,0 +1,8 @@
+﻿class Circle
+{
+    const double PI = 3.14;
+    static void Main(string[] args)
+    {
+        PI = 1.28;  //! This line will generate a compile-time error because PI is a constant and cannot be reassigned.
+    }
+}
